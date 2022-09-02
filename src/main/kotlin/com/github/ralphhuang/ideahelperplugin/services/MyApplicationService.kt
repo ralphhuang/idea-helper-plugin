5863e317eef9758fc4f9bbdd1cb238697fd9159e
GitHub Action <action@github.com>
2022-09-02 08:31:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/ralphhuang/ideahelperplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ralphhuang/ideahelperplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ralphhuang.ideahelperplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ralphhuang.ideahelperplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ralphhuang/ideahelperplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ralphhuang/ideahelperplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ralphhuang.ideahelperplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ralphhuang.ideahelperplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
